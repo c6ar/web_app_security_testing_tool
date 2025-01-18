32164ea03bdfec61e3d8394606a24480be3c1d55
lfalisz <lukasz.falisz@gmail.com>
2025-01-18 02:27:30 +0100

    Adding PowerShell script and its exec counterpart to run main.py using python.

---
+++ WASTT.ps1
@@ -0,0 +1,8 @@
+$scriptDir = [System.IO.Path]::GetDirectoryName([System.Diagnostics.Process]::GetCurrentProcess().MainModule.FileName)
+$pythonFilePath = Join-Path -Path $scriptDir -ChildPath "main.py"
+
+if (Test-Path -Path $pythonFilePath) {
+    Start-Process "python" -ArgumentList $pythonFilePath -WindowStyle Hidden
+} else {
+    Write-Error "The file 'main.py' does not exist at the specified path."
+}
